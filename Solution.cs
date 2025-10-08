5cbe62329de37257c17d096b5ba624dce13b22ac
Lachezar Kutsarov <lkutsarov@gmail.com>
2025-10-08 17:59:31 +0200

    Create Solution.cs

---
+++ Solution.cs
@@ -0,0 +1,45 @@
+
+using System;
+
+public class Solution
+{
+    public int[] SuccessfulPairs(int[] spells, int[] potions, long success)
+    {
+        Array.Sort(potions);
+        int[] successPairs = new int[spells.Length];
+
+        for (int i = 0; i < spells.Length; ++i)
+        {
+            if ((long)potions[potions.Length - 1] * spells[i] < success)
+            {
+                continue;
+            }
+            int minSuccessIndex = binarySearchMinSuccessIndex(potions, spells[i], success);
+            int numberOfSuccessPairs = potions.Length - minSuccessIndex;
+            successPairs[i] = numberOfSuccessPairs;
+        }
+
+        return successPairs;
+    }
+
+    private int binarySearchMinSuccessIndex(int[] potions, int spell, long success)
+    {
+        int left = 0;
+        int right = potions.Length - 1;
+
+        while (left <= right)
+        {
+            int middle = left + (right - left) / 2;
+
+            if ((long)potions[middle] * spell < success)
+            {
+                left = middle + 1;
+            }
+            else
+            {
+                right = middle - 1;
+            }
+        }
+        return left;
+    }
+}
